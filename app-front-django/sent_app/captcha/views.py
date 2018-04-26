4caada8a68ac45f3496d279d3553e7502f7998c2
ancap <mathiaspeterhorst@gmail.com>
2018-04-26 15:52:19 +0200

    finished captcha system

--- app-front-django/sent_app/captcha/views.py
+++ app-front-django/sent_app/captcha/views.py
@@ -8,6 +8,14 @@ from captcha.models import Phrase
 #python imports
 import random
 
+def set_session(request):
+	request.session['inputName'] = request.POST.get("inputName")
+	request.session["inputEmail"] = request.POST.get("inputEmail")
+	request.session["address1"] = request.POST.get("address1")
+	request.session["address2"] = request.POST.get("address2")
+	request.session["city"] = request.POST.get("city")
+	request.session["dialect"] = request.POST.get("dialect")
+
 # Create your views here.
 def index(request):
 	return render(request, "captcha/amazon.html", locals())
@@ -18,6 +26,8 @@ def login(request):
 def captcha(request):
 	if request.method == "POST":
 		loginForm = LoginForm(request.POST)
+		set_session(request)
+		"""
 		name = request.POST.get("inputName")
 		email = request.POST.get("inputEmail")
 		password = request.POST.get("inputPassword")
@@ -25,13 +35,18 @@ def captcha(request):
 		adress2 = request.POST.get("address2")
 		city = request.POST.get("city")
 		dialect = request.POST.get("dialect")
-		phrases = Phrase.objects.all()
-		a  = 42
-		while a != 0:
-			sentence= random.choice(phrases)
-			if sentence.sentiment == "None":
-				break
-			a -= 1
+		"""
+	phrases = Phrase.objects.all()
+	if request.session.has_key("inputName"):
+		name = request.session["inputName"]
+	if request.session.has_key("inputName"):
+		dialect = request.session["dialect"]
+	a  = 42
+	while a != 0:
+		sentence = random.choice(phrases)
+		if sentence.sentiment == "None":
+			break
+		a -= 1
 	return render(request, "captcha/captcha.html", locals())
 
 def success(request):
@@ -39,6 +54,6 @@ def success(request):
 		QU'IL NE SOIT PAR REPROPOSE ET ON EST BON"""
 	if request.method == "POST":
 		answer = request.POST.get("sent")
+		if answer == "idk":
+			return redirect("/captcha/captcha/")
 	return redirect("https://www.amazon.fr/gp/cart/view.html/ref=nav_cart")
-#return redirect("https://www.amazon.fr/")
-#return render(request, "captcha/success.html", locals())
