668d54bfd39c29e76d2781c8009a45ee34a79feb
Abdeljalil Naceur <contact@naceur-abdeljalil.com>
2018-04-28 10:40:09 +0200

    Django-update

--- app-front-django/sent_app/captcha/views.py
+++ app-front-django/sent_app/captcha/views.py
@@ -1,11 +1,28 @@
+#django imports
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import template
 from captcha.forms import LoginForm
+from captcha.models import Phrase
+from sent_app.settings import BASE_DIR, STATICFILES_DIR, STATIC_URL
+
+#python imports
+import random
+import os
+
+def set_session(request):
+	request.session['inputName'] = request.POST.get("inputName")
+	request.session["inputEmail"] = request.POST.get("inputEmail")
+	request.session["address1"] = request.POST.get("address1")
+	request.session["address2"] = request.POST.get("address2")
+	request.session["city"] = request.POST.get("city")
+	request.session["dialect"] = request.POST.get("dialect")
 
 # Create your views here.
 def index(request):
-	return render(request, "captcha/index.html", locals())
+#amazon_fichiers = os.path.join(BASE_DIR, "templates/captcha/amazon_fichiers/")
+	amazon_fichier = os.path.join(STATIC_URL, "amazon_fichiers/")
+	return render(request, "captcha/amazon.html", locals())
 
 def login(request):
 	return render(request, "captcha/login.html", locals())
@@ -13,14 +30,25 @@ def login(request):
 def captcha(request):
 	if request.method == "POST":
 		loginForm = LoginForm(request.POST)
-		name = request.POST.get("inputName")
-		email = request.POST.get("inputEmail")
-		password = request.POST.get("inputPassword")
-		address = request.POST.get("adress1")
-		adress2 = request.POST.get("adress2")
-		city = request.POST.get("city")
-		dialect = request.POST.get("dialect")
+		set_session(request)
+	phrases = Phrase.objects.all()
+	if request.session.has_key("inputName"):
+		name = request.session["inputName"]
+	if request.session.has_key("inputName"):
+		dialect = request.session["dialect"]
+	a  = 42
+	while a != 0:
+		sentence = random.choice(phrases)
+		if sentence.sentiment == "None":
+			break
+		a -= 1
 	return render(request, "captcha/captcha.html", locals())
 
 def success(request):
-	return render(request, "captcha/success.html", locals())
+	""" PLUS QU'A UPDATE LA BDD AVEC LA NOUVELLE VALEUR DU SENTIMENT POUR
+		QU'IL NE SOIT PAR REPROPOSE ET ON EST BON"""
+	if request.method == "POST":
+		answer = request.POST.get("sent")
+		if answer == "idk":
+			return redirect("/captcha/captcha/")
+	return redirect("https://www.amazon.fr/gp/cart/view.html/ref=nav_cart")
--- app-front-django/sent_app/dictionnary/models.py
+++ app-front-django/sent_app/dictionnary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Word(models.Model):
+	word = models.CharField(max_length = 200)
+	dialect = models.CharField(max_length = 50)
+	key = models.IntegerField(unique = True)
+	translated = models.BooleanField(default=False)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.word
